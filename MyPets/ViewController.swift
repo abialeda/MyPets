fde8930e114a0ac65d361b498d880de24ccf842f
Ade <ade@iMac-de-milab-2.local>
2017-07-08 10:46:02 -0500

    initial commint

---
+++ MyPets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyPets
+//
+//  Created by Ade on 08/07/17.
+//  Copyright © 2017 Ade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
